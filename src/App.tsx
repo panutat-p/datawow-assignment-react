d3c96855b86e3cac35f7ad0a0bf57b8da68fda40
panutat-p <pp.panutat@gmail.com>
2023-05-03 05:02:32 +0700

    feat: colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import styled from 'styled-components'
 
-import Button from './components/Button.tsx'
+import TodoList from './components/TodoList.tsx'
 
 function App() {
   return (
-    <Container>
-      <div>
-        <Button>Click me!</Button>
-      </div>
-    </Container>
+    <Background>
+      <Card>
+        <TodoList />
+      </Card>
+    </Background>
   )
 }
 
-const Container = styled.div`
+const Background = styled.div`
+  background-color: #e5e5e5;
+  padding: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
+`
+
+const Card = styled.div`
+  background-color: #f5f5f5;
+  width: 500px;
+  height: 600px;
+  padding: 16px;
+  border-radius: 4px;
 `
 
 export default App
